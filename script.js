d397a29afa0d047a33f66fad1775777155e799aa
MalikSweed <>
2021-06-06 02:37:19 +0000

    adding js

---
+++ script.js
@@ -0,0 +1,35 @@
+
+alert("Hello football fans")
+// var lewandowski;
+// var de bruyn;
+// var haaland;
+// var cristiano ronaldo; 
+// var mbappe;
+// var van dijk;
+// var kimmich;
+// var mohamed salah;
+// var neymar;
+var messi;
+var person = prompt("who is your favorite player?");
+
+if (person = messi){
+  alert("your favorite player in our list ^_^.")}
+// }else if (person =  mohamed salah){
+//   alert("your favorite player in our list ^_^.")
+// } else if (person =  mohamed salah){
+//   alert("your favorite player in our list ^_^.")
+// }else if (person =  mohamed salah){
+//   alert("your favorite player in our list ^_^.")
+// }else if (person =  mohamed salah){
+//   alert("your favorite player in our list ^_^.")
+// }else if (person =  mohamed salah){
+//   alert("your favorite player in our list ^_^.")
+// }else if (person =  mohamed salah){
+//   alert("your favorite player in our list ^_^.")
+// }else if (person =  mohamed salah){
+//   alert("your favorite player in our list ^_^.")
+// }else if (person =  mohamed salah){
+//   alert("your favorite player in our list ^_^.")
+// }else if (person =  mohamed salah){
+//   alert("your favorite player in our list ^_^.")
+// }else alert{"unfortunately your favorite player is not on the list"}
